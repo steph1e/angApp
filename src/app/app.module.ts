0038cc4ac995c055fe0a8a66cf41dcef268c0014
steph1e <s.bontsi@gmail.com>
2017-02-23 09:43:05 +0200

    angApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MemberListComponent } from './member-list/member-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberListComponent
   ],
   imports: [
     BrowserModule,
